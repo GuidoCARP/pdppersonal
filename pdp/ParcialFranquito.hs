d98dee0b771f470bc957ec9d7963924a42b5ce61
Guido Bassi <gbassi@frba.utn.edu.ar>
2024-06-03 16:17:30 -0300

    termino parcial franco

--- pdp/ParcialFranquito.hs
+++ pdp/ParcialFranquito.hs
@@ -8,7 +8,7 @@ stilgar :: Fremen
 stilgar = UnFremen "stilgar" 150 ["Guia"] 101
 
 otroFremen :: Fremen
-otroFremen= UnFremen "otroFremen" 2 ["Guia","Domador"] 5 
+otroFremen= UnFremen "otroFremen" 150 ["Guia","Domador"] 5 
 
 tribu :: [Fremen]
 tribu=[stilgar,otroFremen]
@@ -16,15 +16,23 @@ tribu=[stilgar,otroFremen]
 nuevoReconocimiento :: Fremen -> Fremen
 nuevoReconocimiento fremen = fremen { reconocimientos=reconocimientos fremen+1 } 
 
-candidato :: [Fremen] -> Bool
-candidato  = any (\fremen -> "Domador" `elem` titulos fremen && tolerancia fremen > 100)
+unCandidato :: Fremen -> Bool
+unCandidato fremen = elem "Domador" (titulos fremen) && tolerancia fremen > 100
 
-{-Hallar al Elegido: Es el Fremen de la tribu que más reconocimientos 
-tenga entre los candidatos a serlo. -}
+hayElegido :: [Fremen] -> Bool
+hayElegido = any unCandidato
 
-elgido:: [Fremen] -> Fremen
-elgido fremen = maximum reconocimientos 
+posiblesElegidos :: [Fremen] -> [Fremen]
+posiblesElegidos = filter unCandidato
 
+obtenerMayorDeLista :: [Fremen] -> Fremen
+obtenerMayorDeLista [a] = a
+obtenerMayorDeLista (x:y:xs)
+    | reconocimientos x > reconocimientos y = obtenerMayorDeLista (x:xs)
+    | otherwise = obtenerMayorDeLista (y:xs)
+
+hallarElegido :: [Fremen] -> Fremen
+hallarElegido tribu = obtenerMayorDeLista (posiblesElegidos tribu)  
 data Gusano = UnGusano{
     longitud::Float,
     hidratacion::Int,
@@ -56,32 +64,15 @@ crias [] _ = []
 crias _ [] = []
 crias (gusano1:gusanos1) (gusano2:gusanos2) = longitudCria gusano1 gusano2 : crias gusanos1 gusanos2
 
-{-Domar gusano de arena: Un Fremen puede domar a un gusano de arena si su
- nivel de tolerancia a la Especia es al menos la mitad de la longitud del gusano. 
- Al hacerlo, obtiene el título de "Domador" y su tolerancia a la especia aumenta en 100 unidades. 
-Si no lo puede hacer su tolerancia a la Especia baja un 10%.-}
-
-
 domarGusano :: Fremen -> Gusano -> Fremen
 domarGusano fremen gusano
     | tolerancia fremen >= longitud gusano * 0.5 = fremen {titulos = "Domador" : titulos fremen, tolerancia = tolerancia fremen + 100}
     | otherwise = fremen {tolerancia = tolerancia fremen - tolerancia fremen * 0.1}
 
-{-Destruir gusano de arena: Un Fremen puede destruir a un gusano de arena si tiene el
- título de "Domador" y si su nivel de tolerancia a la Especia es menor que
-  la mitad de la longitud del gusano. Al hacerlo, recibe un 
-reconocimiento y su tolerancia a la especia aumenta en 100 unidades. 
-Si no lo logra, su especia baja un 20%. -}
-
 destruirGusano :: Fremen -> Gusano -> Fremen
 destruirGusano fremen gusano
     | "Domador" `elem` titulos fremen && tolerancia fremen < longitud gusano * 0.5 = fremen {reconocimientos = reconocimientos fremen + 1, tolerancia = tolerancia fremen + 100}
     | otherwise = fremen {tolerancia= tolerancia fremen - tolerancia fremen * 0.2}
-
-{-Inventada: Inventar otra misión que un Fremen pueda hacer con un gusano,
- que también se pueda realizar dependiendo de cómo sea el 
-gusano en relación al Fremen y que provoque consecuencias diferentes sobre el 
-Fremen si lo logra o no.-}
 
 misionInventada :: Fremen -> Gusano -> Fremen
 misionInventada fremen gusano
@@ -90,25 +81,38 @@ misionInventada fremen gusano
     | otherwise = fremen {tolerancia= tolerancia fremen - tolerancia fremen * 0.8}
 
 
-
-
-{-Simular la realización colectiva de una misión: Dada una tribu, una misión cualquiera 
-y un gusano de arena, hacer que cada uno de los Fremen de la tribu intenten llevar a cabo
- la misión con dicho gusano, obteniendo cómo queda la tribu en consecuencia.
- 
-Averiguar, para una tribu, una misión y un gusano, si el hecho de realizarla colectivamente
- haría que el elegido de la tribu fuera un Fremen diferente al que hubieran elegido 
- previamente.
--}
-
 type Mision = Fremen -> Gusano -> Fremen
 type Tribu = [Fremen]
+
 realizarMisionColectiva ::  Tribu -> Mision -> Gusano -> Tribu
 realizarMisionColectiva tribu mision gusano = map (\fremen -> mision fremen gusano) tribu
 
+mismoElegido :: Tribu -> Mision -> Gusano -> Bool  
+mismoElegido tribu mision gusano = reconocimientos (hallarElegido tribu) == reconocimientos (hallarElegido (realizarMisionColectiva tribu mision gusano))
 
---elegido:: Tribu -> Fremen
---elegido = maximum 
+--Punto 4
+--a)Qué pasaría con una tribu de infinitos Fremen al realizar una mision colectiva?
+--Al realizar una mision con una lista infinita, Haskell no podrá terminar de realizar la operacion
+--ya que como la lista es infinita, debe reccorrer todos los elementos para tener un resultado final
 
---cambiaElegido :: Tribu -> Mision -> Gusano -> Bool
---cambiaElegido tribu mision gusano = elegido tribu /= elegido (realizarMisionColectiva tribu mision gusano)
+--ejemplo: ghci> realizacionColectiva (cycle tribu1) domarGusano gusano1
+--[UnFremen {nombre = "Stilgar", toleranciaEspecia = 250.0, titulos = ["Guia","Domador","Domador"], cantReconocientos = 3},
+--UnFremen {nombre = "Pedro", toleranciaEspecia = 1100.0, titulos = ["Domador","Guia","Guerrero","Domador"], cantReconocientos = 2},
+--UnFremen {nombre = "Stilgar", toleranciaEspecia = 250.0, titulos = ["Guia","Domador","Domador"], cantReconocientos = 3},
+--UnFremen {nombre = "Pedro", toleranciaEspecia = 1100.0, titulos = ["Domador","Guia","Guerrero","Domador"], cantReconocientos = 2}, ... y sigue infinitamente
+
+
+--b)Qué pasaría con una tribu de infinitos Fremen al querer saber si hay algún candidato a ser elegido?
+--Debido a que haskell es peresozo, al realizar la operacion any algunCandidato lista, Haskell no necesita recorrer toda la lista
+--porque al encontrar un candidato que ya es true, este devuelve True y no sigue recorriendo la lista
+
+--ejemplo: ghci> algunCandidatoLista (cycle tribu1)
+--               True
+
+--c)Qué pasaría con una tribu de infinitos Fremen al encontrar al elegido?
+--Al igual que en el punto a, Haskell no podrá terminar de realizar la operacion ya que debería recorrer todos los elementos
+--de la lista e ir comparando la cantidad de reconocimientos de cada uno para saber cual es el elegido.
+
+--ejemplo: ghci> hallarElegido (cycle tribu1)
+--               ... (no devuelve nada porque no termina de realizar la operacion)
+
